ea30d4af66b94a2c6d6f76d9b584fd8477ea2faa
Putri Afrah <putriafrah29@gmail.com>
2015-10-30 16:36:15 +0700

    no message

---
+++ 01-Vector/01-Vector/Vector3D.java
@@ -0,0 +1,26 @@
+public class Vector3D {
+
+    double x,y,z;
+
+    public double x()
+    {
+        return x;
+    } 
+
+    public double y()
+    {
+        return y;
+    }
+
+    public double z()
+    {
+        return z;
+    }
+
+    public Vector3D (double x, double y, double z) {
+        this.x=x;
+        this.y=y;
+        this.z=z;
+
+    }
+}
