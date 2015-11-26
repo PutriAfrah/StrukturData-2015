5857cb36af291da062c7aee4ba514b0567f9de94
Putri Afrah <putriafrah29@gmail.com>
2015-11-26 11:27:27 +0700

    no message

---
+++ 18-CariPrima-Tugas/18-CariPrima-Tugas/prima.java
@@ -0,0 +1,25 @@
+public class prima
+{   
+   //  int prima [] = new int[1000000];
+
+    /**
+     * Constructor for objects of class prime
+     */
+   public prima()
+   {   int angka = 90;
+       
+       for(int j=1;j<angka;j++){
+       int periksa=0;
+       for(int i=2;i<j;i++){
+         if((j%i)==0){
+           periksa=1;
+           break;
+         }
+       }
+       
+       if (periksa==0){
+           System.out.println(j);
+       }
+      }
+   }
+}
